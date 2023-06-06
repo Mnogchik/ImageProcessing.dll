7eb461ddcf9df3659bd06b6545829ec051b5c73f
Sergey <polozhencevsergei@mail.ru>
2023-06-06 17:50:34 +0600

    made class static

--- ImageProcessing.cs
+++ ImageProcessing.cs
@@ -8,88 +8,77 @@ namespace ImageProcessor
     /// <summary>
     /// Класс для обработки фона изображения
     /// </summary>
-    public class ImageProcessing
+    public static class ImageProcessing
     {
-        private readonly Bitmap image;
-        /// <summary>
-        /// Чувствительность обработки изображения; отвечает за то, насколько цвет фона может отличаться от выбранного цвета.
-        /// </summary>
-        public readonly int threshold;
-
-        
-        /// <exception cref="ArgumentOutOfRangeException">Выбрасывается, если threshold выходит из диапазона [0, 442].</exception>
-        public ImageProcessing(Bitmap image, int threshold)
-        {
-            if (threshold < 0 || threshold > 442)
-                throw new ArgumentOutOfRangeException("threshold", "Чувствительность должна быть в пределах от 0 до 442.");
-
-            this.image = image;
-            this.threshold = threshold;
-        }
-
-        /// <exception cref="ArgumentOutOfRangeException">Выбрасывается, если threshold выходит из диапазона [0, 442].</exception>
-        public ImageProcessing(Image image, int threshold) : this(new Bitmap(image), threshold) { }
-
-
         /// <summary>
         /// Заменяет пиксели фона на прозрачные
         /// </summary>
+        /// <param name="image">Изображение для обработки</param>
         /// <param name="backgroundColor">Цвет фона, который надо убрать</param>
+        /// <param name="threshold">Чувствительность обработки (от 0 до 442 включительно)</param>
         /// <returns>Bitmap с прозрачными пикселями фона</returns>
-        public Bitmap DeleteBackground(Color backgroundColor)
+        public static Bitmap DeleteBackground(Bitmap image, Color backgroundColor, int threshold)
         {
-            List<Pixel> pixels = GetPixels(); // список всех пикселей картинки
-            List<Pixel> background = GetBackground(pixels, backgroundColor); // список пикселей заднего фона
+            Bitmap _image = new Bitmap(image);
+
+            List<Pixel> background = GetBackground(_image, backgroundColor, threshold);
 
             for (int i = 0; i < background.Count; i++)
             {
-                image.SetPixel(background[i].Point.X, background[i].Point.Y, Color.Transparent);
+                _image.SetPixel(background[i].Point.X, background[i].Point.Y, Color.Transparent);
             }
 
-            return image;
+            return _image;
         }
 
 
         /// <summary>
-        /// Заменяет пиксели фона на colorChangeTo.
+        /// Заменяет пиксели фона на <paramref name="colorChangeTo"/>.
         /// </summary>
+        /// <param name="image">Изображение для обработки</param>
         /// <param name="backgroundColor">Цвет фона, который надо поменять</param>
         /// <param name="colorChangeTo">Цвет, на который надо поменять</param>
+        /// <param name="threshold">Чувствительность обработки (от 0 до 442 включительно)</param>
         /// <returns>Bitmap с измененными пикселями фона</returns>
-        public Bitmap MakeSolidBackground(Color backgroundColor, Color colorChangeTo)
+        public static Bitmap MakeSolidBackground(Bitmap image, Color backgroundColor, Color colorChangeTo, int threshold)
         {
-            List<Pixel> pixels = GetPixels();
-            List<Pixel> background = GetBackground(pixels, backgroundColor);
+            Bitmap _image = new Bitmap(image);
+
+            List<Pixel> background = GetBackground(_image, backgroundColor, threshold);
 
             for (int i = 0; i < background.Count; i++)
             {
-                image.SetPixel(background[i].Point.X, background[i].Point.Y, colorChangeTo);
+                _image.SetPixel(background[i].Point.X, background[i].Point.Y, colorChangeTo);
             }
 
-            return image;
+            return _image;
         }
 
         /// <summary>
         /// Осветляет пиксели фона.
         /// </summary>
+        /// <param name="image">Изображение для обработки</param>
         /// <param name="backgroundColor">Цвет фона, который надо осветлить</param>
+        /// <param name="threshold">Чувствительность обработки (от 0 до 442 включительно)</param>
         /// <returns>Bitmap с осветленными пикселями фона</returns>
-        public Bitmap LightenBackground(Color backgroundColor)
+        public static Bitmap LightenBackground(Bitmap image, Color backgroundColor, int threshold)
         {
-            List<Pixel> pixels = GetPixels();
-            List<Pixel> background = GetBackground(pixels, backgroundColor);
+            // создание нового объекта для исключения ошибок ссылочных типов
+            Bitmap _image = new Bitmap(image); 
+
+            List<Pixel> background = GetBackground(_image, backgroundColor, threshold);
 
             for (int i = 0; i < background.Count; i++)
             {
                 var R = background[i].Color.R + (0.25 * (255 - background[i].Color.R));
                 var G = background[i].Color.G + (0.25 * (255 - background[i].Color.G));
                 var B = background[i].Color.B + (0.25 * (255 - background[i].Color.B));
-                image.SetPixel(background[i].Point.X, background[i].Point.Y, Color.FromArgb((int)R, (int)G, (int)B));
+                _image.SetPixel(background[i].Point.X, background[i].Point.Y, Color.FromArgb((int)R, (int)G, (int)B));
             }
 
-            return image;
+            return _image;
         }
-        private List<Pixel> GetPixels()
+        private static List<Pixel> GetPixels(Bitmap image)
         {
             var pixels = new List<Pixel>(image.Width * image.Height);
 
@@ -103,12 +92,19 @@ namespace ImageProcessor
             return pixels;
         }
 
-        private List<Pixel> GetBackground(List<Pixel> allPixels, Color backgroundColor)
+        private static List<Pixel> GetBackground(List<Pixel> allPixels, Color backgroundColor, int threshold)
         {
             return allPixels.Where(pixel => ColorDistance(pixel.Color, backgroundColor) < threshold).ToList();
         }
 
-        private int ColorDistance(Color c1, Color c2)
+        private static List<Pixel> GetBackground(Bitmap image, Color backgroundColor, int threshold)
+        {
+            List<Pixel> pixels = GetPixels(image);
+
+            return GetBackground(pixels, backgroundColor, threshold);
+        }
+
+        private static int ColorDistance(Color c1, Color c2)
         {
             int rDiff = c1.R - c2.R;
             int gDiff = c1.G - c2.G;
--- Pixel.cs
+++ Pixel.cs
@@ -5,7 +5,7 @@ namespace ImageProcessor
     /// <summary>
     /// Класс, служащий для хранения координаты и цвета пикселя.
     /// </summary>
-    class Pixel
+    internal class Pixel
     {
         public Point Point { get; set; }
         public Color Color { get; set; }
